20c4411f88794815602ab9e68355b448ec956166
Ali Chandra Rizki <153173069+XT32@users.noreply.github.com>
2023-12-18 04:09:59 +0700

    Commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from PyQt5.QtCore import * 
+from PyQt5.QtGui import * 
+from PyQt5.QtQml import * 
+from PyQt5.QtWidgets import *
+from PyQt5.QtQuick import *  
+import sys
+
+class table(QObject):    
+    def __init__(self, parent = None):
+        super().__init__(parent)
+        self.app = QApplication(sys.argv)
+        self.engine = QQmlApplicationEngine(self)
+        self.engine.rootContext().setContextProperty("backend", self)    
+        self.engine.load(QUrl("main.qml"))
+        sys.exit(self.app.exec_())
+
+if __name__ == "__main__":
+    main = table()
